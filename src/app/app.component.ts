c77745e910bb3f0c79e9f2e1874de4ca6c1bcc81
pablocawichii <pablocawichii@gmail.com>
2021-04-09 10:18:02 -0600

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// Face of app
 export class AppComponent implements OnInit {
   title = 'PickMeUp';
 
