a8fc3d0028385d9b29483a66677650c1042f494f
pablocawichii <pablocawichii@gmail.com>
2021-04-09 10:18:36 -0600

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-
+// Routing Module of App
+// Deals with all routing
 const appRoutes: Routes = [
 	{ path: '',  redirectTo: '/map', pathMatch: 'full'},
   {
